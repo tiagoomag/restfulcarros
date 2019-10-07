615cbf757fa70ca3c9e83eb7482dd9c23ac39716
tiagoomag <tiagosantosmagalhaes@hotmail.com>
2019-10-06 23:55:40 -0300

    Implementando security.

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -66,12 +67,11 @@ public class CarrosController {
 	}
 	
 	@PostMapping
+	@Secured({ "ROLE_ADMIN" })
 	public ResponseEntity post(@RequestBody Carro carro) {
 			CarroDTO c = service.insert(carro);
-			
 			URI location = getUri(c.getId());
 			return ResponseEntity.created(location).build();
-		
 	}
 	
 	private URI getUri(Long id) {
--- src/main/java/com/carros/api/exeption/ExceptionConfig.java
+++ src/main/java/com/carros/api/exeption/ExceptionConfig.java
@@ -1,12 +1,15 @@
 package com.carros.api.exeption;
 
-import java.nio.file.AccessDeniedException;
+import java.io.Serializable;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -25,13 +28,14 @@ public class ExceptionConfig extends ResponseEntityExceptionHandler {
     public ResponseEntity errorBadRequest() {
         return ResponseEntity.badRequest().build();
     }
-
-    @ExceptionHandler({
-            AccessDeniedException.class
-    })
-    public ResponseEntity accessDenied() {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error("Acesso negado"));
+    
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    	return new ResponseEntity("Operação não permitida", HttpStatus.METHOD_NOT_ALLOWED);
     }
+    
+    //tratar access denied exception - aula 59
 }
 
 class Error {
